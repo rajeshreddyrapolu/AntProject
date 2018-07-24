cd79da5c30f2ff3d6defc59d69be11e976188909
Rajesh Kumar Reddy Rapolu <rajeshreddy.rapolu@gmail.com>
2018-07-24 14:23:53 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -3,8 +3,8 @@ cd /home/centos/backup
 mkdir -p /home/centos/backup/workdir
 time_stamp=$(date +%Y_%m_%d_%H_%M_%S)
 mkdir -p "/home/centos/backup/workdir/${time_stamp}"
-sudo cp -r /var/www/html/new-swimmiq "/home/centos/backup/workdir/${time_stamp}"
+sudo cp -r /var/www/html/AntProject "/home/centos/backup/workdir/${time_stamp}"
 cd "/home/centos/backup/workdir/${time_stamp}/"
 tar cvfz "$1_${time_stamp}.tgz" *
-s3cmd put "$1_${time_stamp}.tgz"  s3://smiqdbbackup
+s3cmd put "$1_${time_stamp}.tgz"  s3://sneha-new
 sudo rm -rf /home/centos/backup/workdir/
